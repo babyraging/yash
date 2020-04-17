6186cc6cd9218fc4f32516657e954f996b4686dc
summer <daohong.unipd@gmail.com>
2020-04-18 00:12:12 +0200

    Add lex definition completion
    small refactoring

--- src/extension.ts
+++ src/extension.ts
@@ -20,13 +20,14 @@ const legend = (function () {
 	return new vscode.SemanticTokensLegend(tokenTypesLegend, tokenModifiersLegend);
 })();
 
-const keywords =
-	['type', 'option', 'token', 'left', 'right', 'define', 'output', 'precedence', 'nterm', 'destructor', 'union', 'code', 'printer', 'parse-param', 'lex-param'];
-
 export function activate(context: vscode.ExtensionContext) {
-	const provider = new DocumentSemanticAnalyzer();
-	context.subscriptions.push(vscode.languages.registerCompletionItemProvider('bison', provider, '%'));
-	context.subscriptions.push(vscode.languages.registerDocumentSemanticTokensProvider('bison', provider, legend));
+	const yacc = new YaccSemanticAnalyzer();
+	context.subscriptions.push(vscode.languages.registerCompletionItemProvider('bison', yacc, '%'));
+	context.subscriptions.push(vscode.languages.registerDocumentSemanticTokensProvider('bison', yacc, legend));
+
+	const lex = new LexSemanticAnalyzer();
+	context.subscriptions.push(vscode.languages.registerCompletionItemProvider('flex', lex, '%', '{', '<'));
+	context.subscriptions.push(vscode.languages.registerDocumentSemanticTokensProvider('flex', lex, legend));
 }
 
 interface IParsedToken {
@@ -37,18 +38,17 @@ interface IParsedToken {
 	tokenModifiers: string[];
 }
 
-class DocumentSemanticAnalyzer implements vscode.DocumentSemanticTokensProvider, vscode.CompletionItemProvider {
-	private results: Set<string> = new Set();
-	private tokens: Set<string> = new Set();
-	private types: Set<string> = new Set();
-	private startingLine = -1;
-	private endingLine = -1;
-	private invalidRegions: vscode.Range[] = [];
+abstract class SemanticAnalyzer implements vscode.DocumentSemanticTokensProvider, vscode.CompletionItemProvider {
+	protected keywords: string[];
+	protected invalidRegions: vscode.Range[] = [];
+	protected startingLine = -1;
+	protected endingLine = -1;
+
+	constructor(keywords: string[]) {
+		this.keywords = keywords;
+	}
 
 	async provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext): Promise<vscode.CompletionItem[] | vscode.CompletionList> {
-		/**
-		 * Don't suggest anything if the current cursor falls in invalid regions.
-		 */
 		for (let i = 0; i < this.invalidRegions.length; i++) {
 			const range = this.invalidRegions[i];
 			if (range.contains(position)) {
@@ -56,14 +56,74 @@ class DocumentSemanticAnalyzer implements vscode.DocumentSemanticTokensProvider,
 			}
 		}
 
-		let line = document.lineAt(position).text.substr(0, position.character);
 		if (context.triggerKind === vscode.CompletionTriggerKind.TriggerCharacter) {
-			if (line.startsWith('%'))
-				return this._keywordCompletions(document, position);
-
-			return [];
+			return this._handleTrigger(document, position, context.triggerCharacter);
 		}
 
+		return this._rulesCompletion(document, position);
+	}
+
+	async provideDocumentSemanticTokens(document: vscode.TextDocument, token: vscode.CancellationToken): Promise<vscode.SemanticTokens> {
+		const allTokens = this._parseText(document);
+		const builder = new vscode.SemanticTokensBuilder();
+		allTokens.forEach((token) => {
+			builder.push(token.line, token.startCharacter, token.length, this._encodeTokenType(token.tokenType), this._encodeTokenModifiers(token.tokenModifiers));
+		});
+		return builder.build()
+	}
+
+	private _encodeTokenType(tokenType: string): number {
+		if (!tokenTypes.has(tokenType)) {
+			return 0;
+		}
+		return tokenTypes.get(tokenType)!;
+	}
+
+	private _encodeTokenModifiers(strTokenModifiers: string[]): number {
+		let result = 0;
+		for (let i = 0; i < strTokenModifiers.length; i++) {
+			const tokenModifier = strTokenModifiers[i];
+			if (tokenModifiers.has(tokenModifier)) {
+				result = result | (1 << tokenModifiers.get(tokenModifier)!);
+			}
+		}
+		return result;
+	}
+
+	protected _keywordCompletions(document: vscode.TextDocument, position: vscode.Position): vscode.CompletionItem[] | vscode.CompletionList {
+		if (position.line >= this.startingLine) {
+			return [];
+		}
+		return this.keywords.map((keyword) => new vscode.CompletionItem(keyword, vscode.CompletionItemKind.Constructor));
+	}
+
+
+	protected abstract _handleTrigger(document: vscode.TextDocument, position: vscode.Position, character: string | undefined): vscode.CompletionItem[] | vscode.CompletionList;
+	protected abstract _rulesCompletion(document: vscode.TextDocument, position: vscode.Position): vscode.CompletionItem[] | vscode.CompletionList;
+	protected abstract _parseText(document: vscode.TextDocument): IParsedToken[];
+}
+
+class YaccSemanticAnalyzer extends SemanticAnalyzer {
+	private results: Set<string> = new Set();
+	private tokens: Set<string> = new Set();
+	private types: Set<string> = new Set();
+
+	constructor() {
+		super(['type', 'option', 'token', 'left', 'right', 'define', 'output', 'precedence', 'nterm', 'destructor', 'union', 'code', 'printer', 'parse-param', 'lex-param']);
+	}
+
+	protected _handleTrigger(document: vscode.TextDocument, position: vscode.Position, character: string | undefined): vscode.CompletionItem[] | vscode.CompletionList {
+		let line = document.lineAt(position).text.substr(0, position.character);
+		if (character === '%') {
+			if (line.startsWith('%'))
+				return this._keywordCompletions(document, position);
+		}
+
+		return [];
+	}
+
+	protected _rulesCompletion(document: vscode.TextDocument, position: vscode.Position): vscode.CompletionItem[] | vscode.CompletionList {
+		let line = document.lineAt(position).text.substr(0, position.character);
 		/**
 		 * Result suggestion on defining type
 		 */
@@ -95,26 +155,7 @@ class DocumentSemanticAnalyzer implements vscode.DocumentSemanticTokensProvider,
 				return completions;
 			}
 		}
-
 		return [];
-	}
-
-	async provideDocumentSemanticTokens(document: vscode.TextDocument, token: vscode.CancellationToken): Promise<vscode.SemanticTokens> {
-		const allTokens = this._parseText(document);
-		const builder = new vscode.SemanticTokensBuilder();
-		allTokens.forEach((token) => {
-			builder.push(token.line, token.startCharacter, token.length, this._encodeTokenType(token.tokenType), this._encodeTokenModifiers(token.tokenModifiers));
-		});
-		return builder.build();
-	}
-
-	private _keywordCompletions(document: vscode.TextDocument, position: vscode.Position): vscode.CompletionItem[] | vscode.CompletionList {
-		if (position.line >= this.startingLine) {
-			return [];
-		}
-
-		const completions = keywords.map((keyword) => new vscode.CompletionItem(keyword, vscode.CompletionItemKind.Constructor));
-		return completions;
 	}
 
 	private _typeParamCompletions(document: vscode.TextDocument, position: vscode.Position): vscode.CompletionItem[] | vscode.CompletionList {
@@ -129,26 +170,7 @@ class DocumentSemanticAnalyzer implements vscode.DocumentSemanticTokensProvider,
 		return completions;
 	}
 
-
-	private _encodeTokenType(tokenType: string): number {
-		if (!tokenTypes.has(tokenType)) {
-			return 0;
-		}
-		return tokenTypes.get(tokenType)!;
-	}
-
-	private _encodeTokenModifiers(strTokenModifiers: string[]): number {
-		let result = 0;
-		for (let i = 0; i < strTokenModifiers.length; i++) {
-			const tokenModifier = strTokenModifiers[i];
-			if (tokenModifiers.has(tokenModifier)) {
-				result = result | (1 << tokenModifiers.get(tokenModifier)!);
-			}
-		}
-		return result;
-	}
-
-	private _parseText(document: vscode.TextDocument): IParsedToken[] {
+	protected _parseText(document: vscode.TextDocument): IParsedToken[] {
 		let text = document.getText();
 		this.tokens.clear();
 		this.results.clear();
@@ -160,7 +182,7 @@ class DocumentSemanticAnalyzer implements vscode.DocumentSemanticTokensProvider,
 		/**
 		 * Find all invalid regions
 		 */
-		const codeMatcher = /%{[\s\S]*?%}|{[\s\S]*?}/g;
+		const codeMatcher = /%(?=top)?{[\s\S]*?%}|{[\s\S]*?}/g;
 		var cpp;
 		while ((cpp = codeMatcher.exec(text)) !== null) {
 			let start = document.positionAt(cpp.index);
@@ -238,6 +260,105 @@ class DocumentSemanticAnalyzer implements vscode.DocumentSemanticTokensProvider,
 						tokenType: "class",
 						tokenModifiers: []
 					});
+				}
+			}
+		}
+		return r;
+	}
+}
+
+class LexSemanticAnalyzer extends SemanticAnalyzer {
+	private defines: Set<string> = new Set();
+	private states: Set<string> = new Set();
+
+	constructor() {
+		super(['array', 'pointer', 'option', 's', 'x']);
+	}
+
+	protected _handleTrigger(document: vscode.TextDocument, position: vscode.Position, character: string | undefined): vscode.CompletionItem[] | vscode.CompletionList {
+		const completions: vscode.CompletionItem[] = [];
+		let line = document.lineAt(position).text.substr(0, position.character);
+		if (character === '%') {
+			if (line.startsWith('%'))
+				return this._keywordCompletions(document, position);
+		} else if (character === '{') {
+			if (line.charAt(position.character - 2) !== ' ')
+				this.defines.forEach((define) => {
+					completions.push(new vscode.CompletionItem(define, vscode.CompletionItemKind.Class));
+				})
+		} else if (character === '<' && position.line >= this.startingLine && position.line <= this.endingLine) {
+			if (line.match(/^</)) {
+				this.states.forEach((result) => {
+					completions.push(new vscode.CompletionItem(result, vscode.CompletionItemKind.Class));
+				})
+			}
+		}
+
+		return completions;
+	}
+
+	protected _rulesCompletion(document: vscode.TextDocument, position: vscode.Position): vscode.CompletionItem[] | vscode.CompletionList {
+		return [];
+	}
+
+	protected _parseText(document: vscode.TextDocument): IParsedToken[] {
+		this.invalidRegions = [];
+		this.defines.clear();
+
+		let text = document.getText();
+		let r: IParsedToken[] = [];
+		let lines = text.split(/\r\n|\r|\n/);
+
+		/**
+		 * Find all invalid regions
+		 */
+		const codeMatcher = /%(?=top)?{[\s\S]*?%}/g;
+		var cpp;
+		while ((cpp = codeMatcher.exec(text)) !== null) {
+			let start = document.positionAt(cpp.index);
+			let end = document.positionAt(cpp.index + cpp[0].length);
+			this.invalidRegions.push(new vscode.Range(start, end));
+		}
+
+		/**
+		 * Find rule section
+		 */
+		this.startingLine = -1;
+		for (let i = 0; i < lines.length; i++) {
+			const line = lines[i];
+			if (!line.startsWith('%%')) {
+				if (this.startingLine !== -1) {
+					/**
+					 * If we are inside of rules section
+					 */
+					cpp = /(?<=[\t\v\f ]){[\s\S]*?}/.exec(line);
+					if (cpp !== null) {
+						let start = new vscode.Position(i, cpp.index);
+						let end = new vscode.Position(i, cpp.index + cpp[0].length);
+						this.invalidRegions.push(new vscode.Range(start, end));
+					}
+				} else {
+					/**
+					 * If we are inside of definition section
+					 */
+					if (line.startsWith('%s') || line.startsWith('%x')) {
+						const tokenSymbols = line.slice(2).trim().split(" ");
+						tokenSymbols.forEach(token => {
+							if (token.length > 0) {
+								this.states.add(token);
+							}
+						});
+					}
+					const defined = line.match(/^[a-zA-Z0-9_]+/);
+					if (defined !== null) {
+						this.defines.add(defined[0]);
+					}
+				}
+			} else {
+				if (this.startingLine === -1) {
+					this.startingLine = i + 1;
+				} else {
+					this.endingLine = i + 1;
 				}
 			}
 		}
